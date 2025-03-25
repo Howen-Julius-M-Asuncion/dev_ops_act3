a3e3e2e8f772a9cb6ce5a2c8ac41737e6bd2dce3
Howen-Julius-M-Asuncion <159789944+Howen-Julius-M-Asuncion@users.noreply.github.com>
2025-03-25 22:59:03 +0800

    User OptionSettings variables

--- lib/pages/wifi.dart
+++ lib/pages/wifi.dart
@@ -36,103 +36,149 @@ class _WifipageState extends State<Wifipage> {
         middle: Text('Wi-Fi', style: TextStyle(fontWeight: FontWeight.bold)),
       ),
       child: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 8),
-            Form(
-              child: CupertinoFormSection.insetGrouped(
-                margin: EdgeInsets.all(14),
-                children: [
-                  CupertinoListTile(
-                    title: Text('Wi-Fi'),
-                    trailing: CupertinoSwitch(
-                      value: OptionSettings.isWifiOn,
-                      onChanged: (value) {
-                        setState(() {});
-                      },
+        child: ListView(
+            children: [Column(
+              children: [
+                SizedBox(height: 8),
+                Form(
+                  child: CupertinoFormSection.insetGrouped(
+                    margin: EdgeInsets.all(14),
+                    children: [
+                      CupertinoListTile(
+                        title: Text('Wi-Fi'),
+                        trailing: CupertinoSwitch(
+                          value: OptionSettings.isWifiOn,
+                          onChanged: (value) {
+                            setState(() {
+                              if (OptionSettings.airplaneMode) {
+                                // do nothing
+                              } else {
+                                OptionSettings.isWifiOn = !OptionSettings.isWifiOn;
+                                isLoading = true;
+                                Future.delayed(Duration(milliseconds: 3000), () {
+                                  setState(() {
+                                    isLoading = false;
+                                  });
+                                });
+                              }
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                if (OptionSettings.airplaneMode || !OptionSettings.isWifiOn)
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(30, 4, 30, 12),
+                    child: Text(
+                      OptionSettings.airplaneMode
+                          ? 'To enable Wi-Fi, turn off Airplane Mode.'
+                          : 'Wi-Fi networks will be listed here when Wi-Fi is enabled.',
+                      style: TextStyle(fontSize: 16, color: CupertinoColors.systemGrey),
                     ),
                   ),
-                ],
-              ),
-            ),
-            CupertinoListSection.insetGrouped(
-              header: Row(
-                children: [
-                  SizedBox(width: 14),
-                  Text('MY NETWORKS',
-                      style: TextStyle(fontSize: 14, color: CupertinoColors.systemGrey, fontWeight: FontWeight.w100)),
-                  SizedBox(width: 14),
-                  if (isLoading) CupertinoActivityIndicator(),
-                ],
-              ),
-              children: savedNetworks.map((network) {
-                return CupertinoListTile(
-                  title: Text(network['name']),
-                  leading: network['isConnected'] ? Icon(CupertinoIcons.check_mark) : Text(''),
-                  additionalInfo: Row(
-                    children: [
-                      if(network['isSecured'])
-                        Icon(
-                          CupertinoIcons.lock_fill,
-                          size: 16,
-                          color: CupertinoColors.label,
+                if (OptionSettings.isWifiOn)
+                  CupertinoListSection.insetGrouped(
+                    header: Row(
+                      children: [
+                        SizedBox(width: 14),
+                        Text('MY NETWORKS',
+                            style: TextStyle(fontSize: 14, color: CupertinoColors.systemGrey, fontWeight: FontWeight.w100)),
+                        SizedBox(width: 14),
+                        if (isLoading) CupertinoActivityIndicator(),
+                      ],
+                    ),
+                    children: isLoading
+                        ? []
+                        : savedNetworks.map((network) {
+                      return CupertinoListTile(
+                        title: Text(network['name']),
+                        leading: network['isConnected'] ? Icon(CupertinoIcons.check_mark) : Text(''),
+                        additionalInfo: Row(
+                          children: [
+                            if(network['isSecured'])
+                              Icon(
+                                CupertinoIcons.lock_fill,
+                                size: 16,
+                                color: CupertinoColors.label,
+                              ),
+                            SizedBox(width: 5,),
+                            Icon(
+                              CupertinoIcons.wifi,
+                              size: 16,
+                              color: CupertinoColors.label,
+                            ),
+                          ],
                         ),
-                      SizedBox(width: 5,),
-                      Icon(
-                        CupertinoIcons.wifi,
-                        size: 16,
-                        color: CupertinoColors.label,
-                      ),
-                    ],
+                        trailing: CupertinoButton(
+                          padding: EdgeInsets.zero,
+                          child: Icon(CupertinoIcons.info_circle),
+                          onPressed: () {},
+                        ),
+                      );
+                    }).toList(),
                   ),
-                  trailing: CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    child: Icon(CupertinoIcons.info_circle),
-                    onPressed: () {},
+                SizedBox(height: 18),
+                if (OptionSettings.isWifiOn)
+                  CupertinoListSection.insetGrouped(
+                    header: Row(
+                      children: [
+                        SizedBox(width: 14),
+                        Text('OTHER NETWORKS',
+                            style: TextStyle(fontSize: 14, color: CupertinoColors.systemGrey, fontWeight: FontWeight.w100)),
+                        SizedBox(width: 14),
+                        CupertinoActivityIndicator(),
+                      ],
+                    ),
+                    children: isLoading
+                        ? []
+                        : availableNetworks.map((network) {
+                      return CupertinoListTile(
+                        title: Text(network['name']),
+                        leading: Text(''),
+                        additionalInfo: Row(
+                          children: [
+                            if(network['isSecured'])
+                              Icon(
+                                CupertinoIcons.lock_fill,
+                                size: 16,
+                                color: CupertinoColors.label,
+                              ),
+                            SizedBox(width: 5,),
+                            Icon(
+                              CupertinoIcons.wifi,
+                              size: 16,
+                              color: CupertinoColors.label,
+                            ),
+                          ],
+                        ),
+                        trailing: CupertinoButton(
+                          padding: EdgeInsets.zero,
+                          child: Icon(CupertinoIcons.info_circle),
+                          onPressed: () {},
+                        ),
+                      );
+                    }).toList(),
                   ),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 18),
-            CupertinoListSection.insetGrouped(
-              header: Row(
-                children: [
-                  SizedBox(width: 14),
-                  Text('OTHER NETWORKS',
-                      style: TextStyle(fontSize: 14, color: CupertinoColors.systemGrey, fontWeight: FontWeight.w100)),
-                  SizedBox(width: 14),
-                  CupertinoActivityIndicator(),
-                ],
-              ),
-              children: availableNetworks.map((network) {
-                return CupertinoListTile(
-                  title: Text(network['name']),
-                  leading: Text(''),
-                  additionalInfo: Row(
-                    children: [
-                      if(network['isSecured'])
-                      Icon(
-                        CupertinoIcons.lock_fill,
-                        size: 16,
-                        color: CupertinoColors.label,
-                      ),
-                      SizedBox(width: 5,),
-                      Icon(
-                        CupertinoIcons.wifi,
-                        size: 16,
-                        color: CupertinoColors.label,
-                      ),
-                    ],
+                SizedBox(height: 18,),
+                if (OptionSettings.isWifiOn && !isLoading)
+                  CupertinoListSection.insetGrouped(
+                      children: [
+                        CupertinoListTile(
+                          title: Text('Aks to Join Networks'),
+                          additionalInfo: Text('Notify', style: TextStyle(color: CupertinoColors.systemGrey),),
+                          trailing: Icon(CupertinoIcons.chevron_forward, color: CupertinoColors.systemGrey,),
+                        )
+                      ]
                   ),
-                  trailing: CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    child: Icon(CupertinoIcons.info_circle),
-                    onPressed: () {},
+                if (OptionSettings.isWifiOn && !isLoading)
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(30, 4, 30, 12),
+                      child: Text('Known networks will be joined automatically. If no known networks are available, you will be notified of available networks.' ,style: TextStyle(fontSize: 12, color: CupertinoColors.systemGrey), )
                   ),
-                );
-              }).toList(),
-            ),
-          ],
+              ],
+            ),]
         ),
       ),
     );
