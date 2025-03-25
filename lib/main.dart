50944464e424bb1f83a086a1f9be15fc9fa2f623
John Michael Goco <116796110+John-Michael-Goco@users.noreply.github.com>
2025-03-25 23:11:07 +0800

    Update styling

--- lib/main.dart
+++ lib/main.dart
@@ -25,133 +25,203 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        backgroundColor: CupertinoColors.secondarySystemBackground,
-        navigationBar: CupertinoNavigationBar(
-          middle: Text('Settings', style: TextStyle(fontWeight: FontWeight.bold)),
-        ),
-        child: SafeArea(
-            child: Column(
-              children: [
-                Expanded(
-                    child: ListView(
-                      children: [
-                        CupertinoListTile(
-                          title: Text('Airplane Mode'),
-                          leading: Container(
-                              padding: EdgeInsets.all(5),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: CupertinoColors.systemOrange),
-                              child: Icon(CupertinoIcons.airplane, color: CupertinoColors.white)
+      backgroundColor: CupertinoColors.secondarySystemBackground,
+      // navigationBar: CupertinoNavigationBar(
+      //   middle: Text('Settings', style: TextStyle(fontWeight: FontWeight.bold)),
+      // ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    color: CupertinoColors.secondarySystemBackground,
+                    margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    height: 100,
+                    child: Row(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [Padding(padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                        child: Text('Settings', style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25),),
+                      )],
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.fromLTRB(20, 8, 20, 0), child: CupertinoSearchTextField(enabled: false,),),
+                  CupertinoListSection.insetGrouped(
+                    dividerMargin: 25,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Row(children: [
+                          Expanded(
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  height: 75,
+                                  width: 75,
+                                  child: ClipOval(
+                                    child: Image.asset(
+                                      'images/profile.jpg',
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 16),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'HJR AGS',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                      SizedBox(height: 4),
+                                      Text(
+                                        'Apple Account, iCloud, and more',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 14,
+                                          color: CupertinoColors.inactiveGray,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                CupertinoButton(child: Icon(CupertinoIcons.chevron_forward, color: CupertinoColors.systemGrey2), onPressed: (){}),
+                              ],
+                            ),
                           ),
-                          leadingSize: 32,
-                          trailing: CupertinoSwitch(
-                              value: OptionSettings.airplaneMode,
-                              onChanged: (value) {
-                                setState(() {
-                                  OptionSettings.airplaneMode = value;
-                                  isLoading = true;
+                        ],),
+                      )
+                    ],
+                  ),
+                  CupertinoListSection.insetGrouped(
+                    dividerMargin: 25,
+                    children: [
+                      CupertinoListTile(
+                        title: Text('Airplane Mode'),
+                        leading: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: CupertinoColors.systemOrange),
+                            child: Icon(CupertinoIcons.airplane, color: CupertinoColors.white)
+                        ),
+                        leadingSize: 32,
+                        trailing: CupertinoSwitch(
+                            value: OptionSettings.airplaneMode,
+                            onChanged: (value) {
+                              setState(() {
+                                OptionSettings.airplaneMode = value;
+                                isLoading = true;
 
-                                  if (OptionSettings.airplaneMode) {
-                                    // Store previous states
-                                    OptionSettings.previousBluetoothState = OptionSettings.isBluetoothOn;
-                                    OptionSettings.previousWifiState = OptionSettings.isWifiOn;
-                                    OptionSettings.previousHotspotState = OptionSettings.isHotspotOn;
+                                if (OptionSettings.airplaneMode) {
+                                  // Store previous states
+                                  OptionSettings.previousBluetoothState = OptionSettings.isBluetoothOn;
+                                  OptionSettings.previousWifiState = OptionSettings.isWifiOn;
+                                  OptionSettings.previousHotspotState = OptionSettings.isHotspotOn;
 
-                                    OptionSettings.isBluetoothOn = false;
-                                    OptionSettings.isWifiOn = false;
-                                    OptionSettings.isHotspotOn = false;
+                                  OptionSettings.isBluetoothOn = false;
+                                  OptionSettings.isWifiOn = false;
+                                  OptionSettings.isHotspotOn = false;
 
-                                    isLoading = false;
-                                  } else {
-                                    // Simulate loading before restoring previous states
-                                    Future.delayed(Duration(milliseconds: 1200), () {
-                                      setState(() {
-                                        isLoading = false;
-                                        OptionSettings.isBluetoothOn = OptionSettings.previousBluetoothState;
-                                        OptionSettings.isWifiOn = OptionSettings.previousWifiState;
-                                        OptionSettings.isHotspotOn = OptionSettings.previousHotspotState;
-                                      });
+                                  isLoading = false;
+                                } else {
+                                  // Simulate loading before restoring previous states
+                                  Future.delayed(Duration(milliseconds: 1200), () {
+                                    setState(() {
+                                      isLoading = false;
+                                      OptionSettings.isBluetoothOn = OptionSettings.previousBluetoothState;
+                                      OptionSettings.isWifiOn = OptionSettings.previousWifiState;
+                                      OptionSettings.isHotspotOn = OptionSettings.previousHotspotState;
                                     });
-                                  }
-                                });
-                              }),
+                                  });
+                                }
+                              });
+                            }),
+                      ),
+                      CupertinoListTile(
+                        title: Text('WiFi'),
+                        leading: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: CupertinoColors.systemBlue),
+                            child: Icon(CupertinoIcons.wifi, color: CupertinoColors.white)
                         ),
-                        CupertinoListTile(
-                          title: Text('WiFi'),
-                          leading: Container(
-                              padding: EdgeInsets.all(5),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: CupertinoColors.systemBlue),
-                              child: Icon(CupertinoIcons.wifi, color: CupertinoColors.white)
-                          ),
-                          leadingSize: 32,
-                          additionalInfo:  OptionSettings.previousWifiState && isLoading
-                              ? CupertinoActivityIndicator() : Text(OptionSettings.isWifiOn ? 'HJR Wifi' : 'Off'),
-                          trailing: Icon(CupertinoIcons.chevron_forward, color: CupertinoColors.systemGrey2),
-                          onTap: () async {
-                            Navigator.push(
-                              context,
-                              CupertinoPageRoute(title: "Settings",builder: (context) => Wifipage()), ).then((value) {setState(() {});}
-                            );
-                          },
+                        leadingSize: 32,
+                        additionalInfo:  OptionSettings.previousWifiState && isLoading
+                            ? CupertinoActivityIndicator() : Text(OptionSettings.isWifiOn ? 'HJR WiFi' : 'Off'),
+                        trailing: Icon(CupertinoIcons.chevron_forward, color: CupertinoColors.systemGrey2),
+                        onTap: () async {
+                          Navigator.push(
+                            context,
+                            CupertinoPageRoute(title: "Settings",builder: (context) => Wifipage()), ).then((value) {setState(() {});}
+                          );
+                        },
+                      ),
+                      CupertinoListTile(
+                        title: Text('Bluetooth'),
+                        leading: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: CupertinoColors.systemBlue),
+                            child: Icon(CupertinoIcons.bluetooth, color: CupertinoColors.white)
                         ),
-                        CupertinoListTile(
-                          title: Text('Bluetooth'),
-                          leading: Container(
-                              padding: EdgeInsets.all(5),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: CupertinoColors.systemBlue),
-                              child: Icon(CupertinoIcons.bluetooth, color: CupertinoColors.white)
-                          ),
-                          leadingSize: 32,
-                          additionalInfo: OptionSettings.previousBluetoothState && isLoading
-                              ? CupertinoActivityIndicator()
-                              : Text(OptionSettings.isBluetoothOn ? 'On' : 'Off'),
-                          trailing: Icon(CupertinoIcons.chevron_forward, color: CupertinoColors.systemGrey2),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              CupertinoPageRoute(builder: (context) => BTpage()), ).then((value) {setState(() {});}
-                            );
-                          },
+                        leadingSize: 32,
+                        additionalInfo: OptionSettings.previousBluetoothState && isLoading
+                            ? CupertinoActivityIndicator()
+                            : Text(OptionSettings.isBluetoothOn ? 'On' : 'Off'),
+                        trailing: Icon(CupertinoIcons.chevron_forward, color: CupertinoColors.systemGrey2),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            CupertinoPageRoute(builder: (context) => BTpage()), ).then((value) {setState(() {});}
+                          );
+                        },
+                      ),
+                      CupertinoListTile(
+                        title: Text('Cellular'),
+                        leading: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: CupertinoColors.systemGreen),
+                            child: Icon(CupertinoIcons.antenna_radiowaves_left_right, color: CupertinoColors.white)
                         ),
-                        CupertinoListTile(
-                          title: Text('Cellular'),
-                          leading: Container(
-                              padding: EdgeInsets.all(5),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: CupertinoColors.systemGreen),
-                              child: Icon(CupertinoIcons.antenna_radiowaves_left_right, color: CupertinoColors.white)
-                          ),
-                          leadingSize: 32,
-                          trailing: Icon(CupertinoIcons.chevron_forward, color: CupertinoColors.systemGrey2),
+                        leadingSize: 32,
+                        trailing: Icon(CupertinoIcons.chevron_forward, color: CupertinoColors.systemGrey2),
+                        onTap: (){}
+                      ),
+                      CupertinoListTile(
+                        title: Text('Personal Hotspot'),
+                        leading: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: CupertinoColors.systemGreen),
+                            child: Icon(CupertinoIcons.personalhotspot, color: CupertinoColors.white)
                         ),
-                        CupertinoListTile(
-                          title: Text('Personal Hotspot'),
-                          leading: Container(
-                              padding: EdgeInsets.all(5),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: CupertinoColors.systemGreen),
-                              child: Icon(CupertinoIcons.personalhotspot, color: CupertinoColors.white)
-                          ),
-                          leadingSize: 32,
-                          additionalInfo: OptionSettings.previousHotspotState && isLoading
-                              ? CupertinoActivityIndicator()
-                              : Text(OptionSettings.isHotspotOn ? 'On' : 'Off'),
-                          trailing: Icon(CupertinoIcons.chevron_forward, color: CupertinoColors.systemGrey2),
-                          onTap: (){},
-                        ),
-                      ],
-                    )
-                )
-              ],
+                        leadingSize: 32,
+                        additionalInfo: OptionSettings.previousHotspotState && isLoading
+                            ? CupertinoActivityIndicator()
+                            : Text(OptionSettings.isHotspotOn ? 'On' : 'Off'),
+                        trailing: Icon(CupertinoIcons.chevron_forward, color: CupertinoColors.systemGrey2),
+                        onTap: (){},
+                      ),
+                    ],
+                  ),
+                ],
+              )
             )
+          ],
         )
+      )
     );
   }
 }
